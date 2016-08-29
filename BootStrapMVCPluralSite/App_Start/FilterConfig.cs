31f2bc9463730931b1a2fdffa35b674f639c509e
Brett Morin <brettmorin2@gmail.com>
2016-08-29 12:56:05 -0700

    Add project files.

---
+++ BootStrapMVCPluralSite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BootStrapMVCPluralSite
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
