e80964c127825257b53b0d455997422675b7f766
Brett Morin <brettmorin2@gmail.com>
2016-09-13 12:50:28 -0700

    Made changes to angular version

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,19 @@
+﻿//home-index.js
+
+function homeIndexController($scope, $http) {
+  $scope.data = [];
+  $scope.isBusy = true;
+
+  $http.get("/api/v1/topics?includeReplies=true")
+  .then(function (result) {
+    //Successful
+    angular.copy(result.data, $scope.data)
+  },
+  function () {
+    //Error
+    alert("could not load topics")
+  })
+  .then(function () {
+    $scope.isBusy = false;
+  });
+}
