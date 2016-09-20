dfedafdf0b9651dd89e449738de9a54b4717ee0e
Brett Morin <brettmorin2@gmail.com>
2016-09-20 13:22:55 -0700

    Added javascript testing

---
+++ MessageBoard.Tests/ClientTest/app.tests.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="d:\gitcode\bootstrapmvcpluralsite\messageboard.tests\scripts\jasmine.js" />
+/// <reference path="../../messageboard/js/myapp.js" />
+
+describe("myapp tests ->", function () {
+
+  it("isDebug", function () {
+    expect(app.isDebug).toEqual(true);
+  });
+
+  it("log", function () {
+    expect(app.log).toBeDefined();
+    app.log("testing");
+  });
+
+});
